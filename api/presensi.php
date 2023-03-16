655262beadd05d6143d3bd30e4adf83eff9a93d7
Richson Sedjie <c14190185@john.petra.ac.id>
2023-03-16 20:44:37 +0700

    api presensi pegawai

--- api/presensi.php
+++ api/presensi.php
@@ -4,15 +4,24 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 require "../Database.php";
-require "../models/Kantor.php";
+require "../models/Presensi.php";
 
 $database = new Database();
 $db = $database->connect();
 
-$kantor = new Kantor($db);
+$presensi = new Presensi($db);
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $res = $kantor->getAllData();
+    if (isset($_GET['id'])) {
+        $res = $presensi->getSingleData($_GET['id']);
+    } else if (isset($_GET['nik'])) {
+        $res = $presensi->getDataByNIKPegawai($_GET['nik']);
+    }else if (isset($_GET['id_kantor'])) {
+        $res = $presensi->getDataByIdKantor($_GET['id_kantor']);
+    } else {
+        $res = $presensi->getAllData();
+    }
+
     if ($res) {
         $result = array(
             'status' => 1,
@@ -21,18 +30,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     } else {
         $result = array(
             'status' => 0,
-            'message' => 'Data Kantor tidak ditemukan'
+            'message' => 'Data presensi tidak ditemukan'
         );
     }
-
     echo json_encode($result);
+
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $result = array(
         'status' => 0,
         'message' => 'Test 1234'
     );
-    if (isset($_POST['nama']) && isset($_POST['alamat'])){
-        $tmp = $kantor->insertKantor($_POST['nama'], $_POST['alamat']);
+
+    if(isset($_POST['nik']) && isset($_POST['id_kantor']) && isset($_POST['tanggal']) && isset($_POST['jam_masuk']) && isset($_POST['foto']) ){
+        $tmp = $presensi->insertPresensiMasukPegawai($_POST['nik'], $_POST['id_kantor'], $_POST['tanggal'], $_POST['jam_masuk'], $_POST['foto']);
+
         if ($tmp == false) {
             $result['status'] = 0;
             $result['message'] = "Data gagal diinput";
@@ -41,51 +52,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $result['status'] = 1;
             $result['message'] = $tmp;
         }
-    } else {
+    }
+    else {
         $result['status'] = 0;
         $result['message'] = "Pastikan semua parameter sudah lengkap";
     }
-    echo json_encode($result);
 
-} else if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
+    echo json_encode($result);
+}else if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
     $result = array(
         'status' => 0,
         'message' => ''
     );
 
     $data = json_decode(file_get_contents("php://input"));
-    if (isset($data->id_kantor) && isset($data->nama) && isset($data->alamat)){
+
+    if (isset($data->id_presensi) && isset($data->jam_keluar)){
         // $kantor->nama = $data->nama;
         // $kantor->alamat = $data->alamat;
-        $tmp = $kantor->updateDetailKantor($data->id_kantor, $data->nama, $data->alamat);
+        $tmp = $presensi->updatePresensiKeluar($data->id_presensi, $data->jam_keluar);
 
         if ($tmp) {
             $result['status'] = 1;
-            $result['message'] = "kantor berhasil diupdate";
+            $result['message'] = "Jam keluar berhasil diupdate";
         } else {
             $result['status'] = 0;
-            $result['message'] = "kantor gagal diupdate";
-        }
-    } else {
-        $result['status'] = 0;
-        $result['message'] = "Pastikan parameter sudah terisi";
-    }
-    echo json_encode($result);
-} else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-    $result = array(
-        'status' => 0,
-        'message' => ''
-    );
-    $data = json_decode(file_get_contents("php://input"));
-    if (isset($data->id)) {
-        $tmp = $kantor->deactivateData($data->id);
-
-        if ($tmp) {
-            $result['status'] = 1;
-            $result['message'] = "Data berhasil dihapus";
-        } else {
-            $result['status'] = 0;
-            $result['message'] = "Data gagal dihapus";
+            $result['message'] = "Jam keluar gagal diupdate";
         }
     } else {
         $result['status'] = 0;
@@ -93,6 +85,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     }
     echo json_encode($result);
 }
+
 else {
     header("HTTP/1.1 400 Bad Request");
     $error = array(
---
+++ models/Presensi.php
@@ -0,0 +1,90 @@
+<?php
+
+class Presensi{
+    private $conn;
+    private $table = 'presensi_pegawai';
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+    public function getAllData() {
+        $query = "SELECT presensi_pegawai.id, presensi_pegawai.nik, pegawai.nama, presensi_pegawai.id_kantor, kantor.nama as lokasi, presensi_pegawai.tanggal, presensi_pegawai.jam_masuk, presensi_pegawai.jam_keluar, presensi_pegawai.foto, presensi_pegawai.status 
+        FROM `presensi_pegawai`
+        LEFT JOIN pegawai ON pegawai.nik = presensi_pegawai.nik
+        LEFT JOIN kantor on kantor.id = presensi_pegawai.id_kantor WHERE presensi_pegawai.status <> 0 ORDER BY presensi_pegawai.tanggal DESC;";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        $row = [];
+        while($data = $stmt->fetch(PDO::FETCH_OBJ))
+            $row[] = $data;
+
+        return $row;
+    }
+    public function getSingleData($id) {
+        $query = "SELECT presensi_pegawai.id, presensi_pegawai.nik, pegawai.nama, presensi_pegawai.id_kantor, kantor.nama as lokasi, presensi_pegawai.tanggal, presensi_pegawai.jam_masuk, presensi_pegawai.jam_keluar, presensi_pegawai.foto, presensi_pegawai.status 
+        FROM `presensi_pegawai`
+        LEFT JOIN pegawai ON pegawai.nik = presensi_pegawai.nik
+        LEFT JOIN kantor on kantor.id = presensi_pegawai.id_kantor WHERE presensi_pegawai.id = ? AND presensi_pegawai.status <> 0 ORDER BY presensi_pegawai.tanggal DESC;";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([(int)$id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $row;
+    }
+
+    public function getDataByNIKPegawai($nik) {
+        $query = "SELECT presensi_pegawai.id, presensi_pegawai.nik, pegawai.nama, presensi_pegawai.id_kantor, kantor.nama as lokasi, presensi_pegawai.tanggal, presensi_pegawai.jam_masuk, presensi_pegawai.jam_keluar, presensi_pegawai.foto, presensi_pegawai.status 
+        FROM `presensi_pegawai`
+        LEFT JOIN pegawai ON pegawai.nik = presensi_pegawai.nik
+        LEFT JOIN kantor on kantor.id = presensi_pegawai.id_kantor WHERE presensi_pegawai.nik = ? AND presensi_pegawai.status <> 0 ORDER BY presensi_pegawai.tanggal DESC;";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([(int)$nik]);
+        $row = [];
+        while($data = $stmt->fetch(PDO::FETCH_OBJ))
+            $row[] = $data;
+
+        return $row;
+    }
+
+    public function getDataByIdKantor($id_kantor) {
+        $query = "SELECT presensi_pegawai.id, presensi_pegawai.nik, pegawai.nama, presensi_pegawai.id_kantor, kantor.nama as lokasi, presensi_pegawai.tanggal, presensi_pegawai.jam_masuk, presensi_pegawai.jam_keluar, presensi_pegawai.foto, presensi_pegawai.status 
+        FROM `presensi_pegawai`
+        LEFT JOIN pegawai ON pegawai.nik = presensi_pegawai.nik
+        LEFT JOIN kantor on kantor.id = presensi_pegawai.id_kantor WHERE presensi_pegawai.id_kantor = ? AND presensi_pegawai.status <> 0 ORDER BY presensi_pegawai.tanggal DESC;";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([(int)$id_kantor]);
+        $row = [];
+        while($data = $stmt->fetch(PDO::FETCH_OBJ))
+            $row[] = $data;
+
+        return $row;
+    }
+
+    public function insertPresensiMasukPegawai($nik, $id_kantor, $tanggal, $jam_masuk, $foto){
+        $query = "INSERT INTO `$this->table` VALUES (DEFAULT, ?, ?, ?, ?, NULL, ?, 1)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([(int)$nik, (int)$id_kantor, $tanggal, $jam_masuk, $foto]);
+
+        if ($stmt->rowCount() > 0) {
+            $query_take = "SELECT * FROM `$this->table` ORDER BY id DESC LIMIT 1";
+            $stmt = $this->conn->prepare($query_take);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $row['id'];
+        } else {
+            return false;
+        }
+    }
+
+    public function updatePresensiKeluar($id, $jam_keluar){
+        $query = "UPDATE `$this->table` SET jam_keluar = ? WHERE id = ? AND status <> 0";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$jam_keluar,(int)$id]);
+        if ($stmt->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
